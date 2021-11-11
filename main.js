2150fbc54c23ee10197fa9760a9f67e66c4ac852
arnav-bucha <85433289+arnav-bucha@users.noreply.github.com>
2021-11-11 17:59:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+//Create a reference for canvas 
+
+//Give specific height and width to the car image
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+
+function add() {
+	//upload car, and background images on the canvas.
+}
+
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	//Define function to move the car upward
+		if  (rover_y >=0){
+			rover_y=rover_y-10;
+			console.log("When up arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+			uploadBackground();
+			uploadrover();
+		}
+}
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
